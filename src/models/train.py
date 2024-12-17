eaad46b9f828ae1d8a9611bafedd6b4cc862014f
Kamal Y H Qashtam <56519029+kamal480@users.noreply.github.com>
2024-12-17 21:23:12 +0400

    Update train.py

--- src/models/train.py
+++ src/models/train.py
@@ -3,7 +3,7 @@ from sklearn.ensemble import RandomForestClassifier
 import pandas as pd
 
 # Load data
-data = pd.read_csv('classification_data.csv')
+data = pd.read_csv('src/models/classification_data.csv')
 X = data.drop(columns=['class'])
 y = data['class']
 
