fee724e54f953a6581a01a1f93d2f99bc478bda4
Kamal Y H Qashtam <kamal480@hotmail.com>
2024-12-18 03:37:58 +0400

    nv

--- src/models/train.py
+++ src/models/train.py
@@ -1,9 +1,10 @@
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 import pandas as pd
+import os
 
 # Load data
-data = pd.read_csv('classification_data.csv')
+data = pd.read_csv('src/models/classification_data.csv')
 X = data.drop(columns=['class'])
 y = data['class']
 
@@ -14,6 +15,12 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 model = RandomForestClassifier(random_state=42)
 model.fit(X_train, y_train)
 
-# Save the trained model
+# Save the trained model-nv
 import joblib
-joblib.dump(model, 'classification_model.pkl')
+output_dir = 'src/models/'
+os.makedirs(output_dir, exist_ok=True)
+joblib.dump(model, os.path.join(output_dir, 'classification_model.pkl'))
+print("Model saved successfully in 'src/models/classification_model.pkl'")
+
+
+joblib.dump(model, 'src/models/classification_model.pkl')
