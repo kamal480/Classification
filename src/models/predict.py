f41eac692514a5e1073e09403bb7071c96805414
Kamal Y H Qashtam <56519029+kamal480@users.noreply.github.com>
2024-12-17 21:36:16 +0400

    Update predict.py

--- src/models/predict.py
+++ src/models/predict.py
@@ -2,11 +2,11 @@ import pandas as pd
 import joblib
 
 # Load data
-data = pd.read_csv('classification_data.csv')
+data = pd.read_csv('src/models/classification_data.csv')
 X = data.drop(columns=['class'])
 
 # Load model
-model = joblib.load('classification_model.pkl')
+model = joblib.load('src/models/classification_model.pkl')
 
 # Predictions
 predictions = model.predict(X)
